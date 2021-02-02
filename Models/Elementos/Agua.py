b7de2bd144702093551f7802b24938bd703fa4c2
Gustavo Silva <gustavospr@hotmail.com>
2021-02-02 00:26:42 -0300

    Criação de pacotes
    Adicionado arquivos Init em cada diretório para fazer eles virarem pacotes.

--- Models/Elementos/Agua.py
+++ Models/Elementos/Agua.py
@@ -1,7 +1,10 @@
+from ..Entidade import entidade
+
 #classe água e suas propriedades
 class Agua:
     #Definição das propriedades da água
-    Entidade = Entidade
+
     Sabor = bool
     Cheiro = bool
     Volume = int
+
