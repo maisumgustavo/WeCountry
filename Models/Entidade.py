29a3acbd1d24fccf14a61735f2424430fbd98395
Gustavo Pinheiro <gustavospr@hotmail.com>
2022-11-19 16:11:48 -0300

    Removing python files because change to rust lang

--- Models/Entidade.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-class Entidade:
-    def __init__(self):
-        
-        self.id = 1
-        self.data_Criacao = datetime.now()
-
--- main.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-from Models.Entidade import Entidade
-
-teste = Entidade(1, datetime.now())
-print(teste.id)
