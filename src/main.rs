13119983a7ff159ec5f1d04db2e076f8fd5c8335
Gustavo Pinheiro <gustavospr@hotmail.com>
2022-11-19 22:37:41 -0300

    Added access page to control login and registry users

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
 };
 use wecountry::ThreadPool;
 
@@ -13,10 +11,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./src/views/hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "./src/views/hello.html")
-        }
+        "GET /access HTTP/1.1" => ("HTTP/1.1 200 OK", "./src/views/access.html"),
         _ => ("HTTP/1.1 404 NOT FOUND", "./src/views/404.html"),
     };
 
